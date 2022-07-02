be261e987a16047f49ca2cfc0590ad7b0b061a5a
DedSec <dedsecodes@gmail.com>
2022-07-03 00:08:52 +0100

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			sports : [
+				{name: "Soccer", votes: 0},
+				{name: "HandBall", votes: 0},
+				{name: "BasketBall", votes: 0},
+				{name: "Tennis", votes: 0}
+			]
+		}
+	}
 
+	vote (i) {
+		let newSports = [...this.state.sports];
+		newSports[i].votes++;
+		function swap(array, i, j) {
+			var temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+		this.setState({sports: newSports});
+		
+	}
+
+	render(){
+		return(
+			<>
+				<h1>Vote For Your Best Sport!</h1>
+				<div className="sports">
+					{
+						this.state.sports.map((lang, i) => 
+							<div key={i} className="sport">
+								<div className="voteCount">
+									{lang.votes}
+								</div>
+								<div className="sportName">
+									{lang.name}
+								</div>
+								<button onClick={this.vote.bind(this, i)}>Click Here</button>
+							</div>
+						)
+					}
+				</div>
+			</>
+		);
+	}
+}
 export default App;
